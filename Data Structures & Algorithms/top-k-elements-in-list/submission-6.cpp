6e9ea569884210dc01d437fc71e23f8d5822b636
Roberto Rodriguez <58539522+roburnto@users.noreply.github.com>
2026-05-04 17:03:09 -0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num] = 1 + freq[num];
+        }
+        vector<vector<int>> buckets(nums.size()+1);
+        
+        for(auto& pair : freq){
+            buckets[pair.second].push_back(pair.first);
+        }
+        vector<int> res;
+        for(int i = buckets.size()-1; i >= 0 ; i--){
+            for(int n : buckets[i]){
+                res.push_back(n);
+            }
+            if (res.size() >= k){
+                return res;
+            }
+        }
+        return res;
+    }
+};
